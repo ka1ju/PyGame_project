3e65c2466da829eb27eac9063b838d6908011c51
Zherebtsov Yaroslav <n0name_off@mail.ru>
2021-12-26 22:51:01 +0700

    обновил

--- windows.py
+++ windows.py
@@ -524,9 +524,9 @@ class MainScreen:
         main_screen_sprites.add(self.img_close)
 
     def render(self, arg):
-        main_screen_sprites.draw(self.screen)
-        if arg != '':
-            arg.draw(self.screen)
+        main_screen_sprites.draw(screen)
+        if ren != '':
+            arg.draw(screen)
 
     def click(self, position):
         if position[1] in range(height // 2 - 50, height // 2 + 50):
@@ -540,13 +540,19 @@ class MainScreen:
             return 'exit'
 
 
+shop_sprites = pygame.sprite.Group()
 
+shop_sprites.add(background)
+
+player_sprites = pygame.sprite.Group()
+wall_sprites = pygame.sprite.Group()
+color_sprites = pygame.sprite.Group()
 
 
 class Shop:
     def __init__(self):
         # спрайты магазина
-        self.type = 'shop'
+        self.type = 'player'
 
         self.img_player_btn = pygame.sprite.Sprite()
         self.img_player_btn.image = pygame.transform.scale(load_image('sprites/img_player_btn.png'), (100, 100))
@@ -605,6 +611,8 @@ class Shop:
         shop_sprites.add(self.img_close)
 
         # спрайты персонажей
+        self.player_1_cost = 1
+
         self.player_1 = pygame.sprite.Sprite()
         self.player_1.image = pygame.transform.scale(load_image('sprites/player_1.png'), (50, 50))
         self.player_1.rect = self.player_1.image.get_rect()
@@ -612,12 +620,107 @@ class Shop:
         self.player_1.rect.y = 150
         player_sprites.add(self.player_1)
 
+        self.buy_player_1_btn = pygame.sprite.Sprite()
+        self.buy_player_1_btn.image = pygame.transform.scale(load_image('sprites/img_buy_btn.png'), (25, 25))
+        self.buy_player_1_btn.rect = self.buy_player_1_btn.image.get_rect()
+        self.buy_player_1_btn.rect.x = 25
+        self.buy_player_1_btn.rect.y = 210
+        player_sprites.add(self.buy_player_1_btn)
+
+        self.buy_player_1 = pygame.sprite.Sprite()
+        self.buy_player_1.image = pygame.transform.scale(load_image('sprites/img_buy.png'), (25, 25))
+        self.buy_player_1.rect = self.buy_player_1.image.get_rect()
+        self.buy_player_1.rect.x = 27
+        self.buy_player_1.rect.y = 210
+        player_sprites.add(self.buy_player_1)
+
+        self.choose_player_1_btn = pygame.sprite.Sprite()
+        self.choose_player_1_btn.image = pygame.transform.scale(load_image('sprites/img_choose_btn.png'), (25, 25))
+        self.choose_player_1_btn.rect = self.choose_player_1_btn.image.get_rect()
+        self.choose_player_1_btn.rect.x = 50
+        self.choose_player_1_btn.rect.y = 210
+        player_sprites.add(self.choose_player_1_btn)
+
+        self.choose_player_1 = pygame.sprite.Sprite()
+        self.choose_player_1.image = pygame.transform.scale(load_image('sprites/img_choose.png'), (15, 15))
+        self.choose_player_1.rect = self.choose_player_1.image.get_rect()
+        self.choose_player_1.rect.x = 55
+        self.choose_player_1.rect.y = 215
+        player_sprites.add(self.choose_player_1)
+
+        self.player_2_cost = 2
+
         self.player_2 = pygame.sprite.Sprite()
         self.player_2.image = pygame.transform.scale(load_image('sprites/player_2.png'), (50, 50))
         self.player_2.rect = self.player_2.image.get_rect()
         self.player_2.rect.x = 125
         self.player_2.rect.y = 150
         player_sprites.add(self.player_2)
+
+        self.buy_player_2_btn = pygame.sprite.Sprite()
+        self.buy_player_2_btn.image = pygame.transform.scale(load_image('sprites/img_buy_btn.png'), (25, 25))
+        self.buy_player_2_btn.rect = self.buy_player_2_btn.image.get_rect()
+        self.buy_player_2_btn.rect.x = 125
+        self.buy_player_2_btn.rect.y = 210
+        player_sprites.add(self.buy_player_2_btn)
+
+        self.buy_player_2 = pygame.sprite.Sprite()
+        self.buy_player_2.image = pygame.transform.scale(load_image('sprites/img_buy.png'), (25, 25))
+        self.buy_player_2.rect = self.buy_player_2.image.get_rect()
+        self.buy_player_2.rect.x = 127
+        self.buy_player_2.rect.y = 210
+        player_sprites.add(self.buy_player_2)
+
+        self.choose_player_2_btn = pygame.sprite.Sprite()
+        self.choose_player_2_btn.image = pygame.transform.scale(load_image('sprites/img_choose_btn.png'), (25, 25))
+        self.choose_player_2_btn.rect = self.choose_player_2_btn.image.get_rect()
+        self.choose_player_2_btn.rect.x = 150
+        self.choose_player_2_btn.rect.y = 210
+        player_sprites.add(self.choose_player_2_btn)
+
+        self.choose_player_2 = pygame.sprite.Sprite()
+        self.choose_player_2.image = pygame.transform.scale(load_image('sprites/img_choose.png'), (15, 15))
+        self.choose_player_2.rect = self.choose_player_2.image.get_rect()
+        self.choose_player_2.rect.x = 155
+        self.choose_player_2.rect.y = 215
+        player_sprites.add(self.choose_player_2)
+
+        self.player_3_cost = 3
+
+        self.player_3 = pygame.sprite.Sprite()
+        self.player_3.image = pygame.transform.scale(load_image('sprites/player_3.png'), (50, 50))
+        self.player_3.rect = self.player_3.image.get_rect()
+        self.player_3.rect.x = 225
+        self.player_3.rect.y = 150
+        player_sprites.add(self.player_3)
+
+        self.buy_player_3_btn = pygame.sprite.Sprite()
+        self.buy_player_3_btn.image = pygame.transform.scale(load_image('sprites/img_buy_btn.png'), (25, 25))
+        self.buy_player_3_btn.rect = self.buy_player_3_btn.image.get_rect()
+        self.buy_player_3_btn.rect.x = 225
+        self.buy_player_3_btn.rect.y = 210
+        player_sprites.add(self.buy_player_3_btn)
+
+        self.buy_player_3 = pygame.sprite.Sprite()
+        self.buy_player_3.image = pygame.transform.scale(load_image('sprites/img_buy.png'), (25, 25))
+        self.buy_player_3.rect = self.buy_player_3.image.get_rect()
+        self.buy_player_3.rect.x = 227
+        self.buy_player_3.rect.y = 210
+        player_sprites.add(self.buy_player_3)
+
+        self.choose_player_3_btn = pygame.sprite.Sprite()
+        self.choose_player_3_btn.image = pygame.transform.scale(load_image('sprites/img_choose_btn.png'), (25, 25))
+        self.choose_player_3_btn.rect = self.choose_player_3_btn.image.get_rect()
+        self.choose_player_3_btn.rect.x = 250
+        self.choose_player_3_btn.rect.y = 210
+        player_sprites.add(self.choose_player_3_btn)
+
+        self.choose_player_3 = pygame.sprite.Sprite()
+        self.choose_player_3.image = pygame.transform.scale(load_image('sprites/img_choose.png'), (15, 15))
+        self.choose_player_3.rect = self.choose_player_3.image.get_rect()
+        self.choose_player_3.rect.x = 255
+        self.choose_player_3.rect.y = 215
+        player_sprites.add(self.choose_player_3)
 
         # спрайты стен
         self.wall_1 = pygame.sprite.Sprite()
@@ -642,25 +745,29 @@ class Shop:
         wall_sprites.add(self.wall_3)
 
     def render(self, arg):
-        shop_sprites.draw(self.screen)
-        if arg != '':
-            arg.draw(self.screen)
+        shop_sprites.draw(screen)
+        if ren != '':
+            arg.draw(screen)
 
     def click(self, position):
-        if self.type == 'shop':
-            if position[1] in range(0, 100):
-                if position[0] in range(0, 100):
-                    return 'player'
-                elif position[0] in range((width - 100) // 2, (width - 100) // 2 + 100):
-                    return 'wall'
-                elif position[0] in range(width - 100, width):
-                    return 'color'
-            elif position[1] in range(height - 50, height):
-                if position[0] in range(width - 50, width):
-                    return 'shop_exit'
-        elif self.type == 'player':
-            if position[1] in range(205, 220):
-                if position[0] in range(50, 100):
+        if position[1] in range(0, 100):
+            if position[0] in range(0, 100):
+                self.type = 'player'
+                return 'player'
+            elif position[0] in range((width - 100) // 2, (width - 100) // 2 + 100):
+                self.type = 'wall'
+                return 'wall'
+            elif position[0] in range(width - 100, width):
+                self.type = 'color'
+                return 'color'
+        elif position[1] in range(height - 50, height):
+            if position[0] in range(width - 50, width):
+                return 'shop_exit'
+        if self.type == 'player':
+            if position[1] in range(210, 235):
+                if position[0] in range(25, 50):
                     return 'buy_player_1'
-                if position[1] in range(150, 200):
+                elif position[0] in range(125, 150):
                     return 'buy_player_2'
+                elif position[0] in range(225, 250):
+                    return 'buy_player_3'
