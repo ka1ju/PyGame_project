98a3986d0c6ebab3fa587cb1640f15d64e422f7d
Zherebtsov Yaroslav <n0name_off@mail.ru>
2022-01-16 23:21:51 +0700

    немного говна убрал

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ screen = pygame.display.set_mode(size)
 running = True
 
 
-def load_image(name, color_key=None):
-    fullname = os.path.join('', name)
+def load_image(file_name, color_key=None):
+    fullname = os.path.join('', file_name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
